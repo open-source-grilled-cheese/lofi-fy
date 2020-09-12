911fc2078f3f6b73e2121e088eeeb79440e0f092
Debian <bitnami@ip-172-26-1-28.us-east-2.compute.internal>
2020-09-12 16:16:59 +0000

    intermediary

--- upload.js
+++ upload.js
@@ -1,8 +1,22 @@
 function submitForm() {
-	var formElem = document.querySelector("form");
-	console.log(formElem);
-	var request = new XMLHttpRequest();
-	console.log(request);
-	request.open("POST", "lofify");
-	request.send(new FormData(formElem));
+	// var formData = $("#formdata").serializeArray();
+	// console.log(document.getElementById("customFile").files[0]);
+	// console.log(JSON.stringify(document.getElementById("customFile").files[0]))
+	// formData.push(document.getElementById("customFile").files[0]);
+	// var d = JSON.stringify(formData);
+	// console.log(d);
+	// $.ajax({
+	// 	type: "POST",
+	// 	url: "/lofify",
+	// 	data: d,
+	// 	success: () => {},
+	// 	dataType: "json",
+	// 	contentType: "application/json"
+	// });
+	 var formElem = document.querySelector("form");
+	 console.log(formElem);
+	 var request = new XMLHttpRequest();
+	 console.log(request);
+	 request.open("POST", "new"); // changed from lofi to test
+	 request.send(new FormData(formElem));
 }
