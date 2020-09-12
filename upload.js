1f816eb86f20e7b6d9f43b42b850d48d405a57b4
Debian <bitnami@ip-172-26-1-28.us-east-2.compute.internal>
2020-09-12 11:49:30 +0000

    add auto form upload

--- upload.js
+++ upload.js
@@ -1,1 +1,8 @@
-console.log("test");
+function submitForm() {
+	var formElem = document.querySelector("form");
+	console.log(formElem);
+	var request = new XMLHttpRequest();
+	console.log(request);
+	request.open("POST", "lofify");
+	request.send(new FormData(formElem));
+}
