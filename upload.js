d45dd8c43632e65d059aecf557bd8bae63dc9200
Debian <bitnami@ip-172-26-1-28.us-east-2.compute.internal>
2020-09-13 03:41:19 +0000

    stuff

--- upload.js
+++ upload.js
@@ -3,6 +3,14 @@ function submitForm() {
 	console.log(formElem);
 	var request = new XMLHttpRequest();
 	console.log(request);
+	// request.open("POST", "new");
 	request.open("POST", "lofify");
+	request.onreadystatechange = function() {
+		if(request.readyState === 4) {
+			if(request.status === 200) {
+				window.location = '/out.html';
+			}
+		}
+	};
 	request.send(new FormData(formElem));
 }
